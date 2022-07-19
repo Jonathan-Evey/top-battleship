e458b078d8eb338f0942220609d7ee8d66c64aba
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-07-19 06:32:10 -0700

    start work on the functions of the game board

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,25 @@
-const Gameboard = () => {};
+const Gameboard = () => {
+	const grid = (gridKey) => {
+		let gridKey = gridKey;
+		let isOpen = true;
+	};
+
+	let gameGrid = [];
+
+	const init = () => {
+		for (let i = 0; i <= 10; i++) {
+			gameGrid.push(grid(`A: ${i}`));
+		}
+	};
+
+	const place = (startingSquare, shipName, direction) => {
+		let newShip = shipName;
+		newShip = Ship(shipKeys.shipName.size);
+		if (direction === horizontal) {
+			for (let i = 0; i < newShip.length; i++)
+				newShip.coordinates.push(startingSquare);
+		}
+	};
+};
 
 module.exports = Gameboard;
