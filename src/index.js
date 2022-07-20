210ea40e32d60c2f0401950b1d84af7bed77cede
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-07-20 09:36:07 -0700

    start of the game play functions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,25 @@ import Ship from "./ship";
 import Gameboard from "./gameboard";
 import Player from "./player";
 import HTML from "./DOMElements";
+
+let playerOne = Gameboard();
+let playerTwo = Gameboard();
+
+let currentPlayer;
+
+function render() {
+	currentPlayer = playerOne;
+	currentPlayer.playGrid.forEach((cell) => {
+		let cellDiv = document.createElement("div");
+		cellDiv.classList.add("cell");
+		cellDiv.innerText = cell;
+		HTML.main.playerGrid.appendChild(cellDiv);
+	});
+}
+
+function startGame() {}
+
+playerOne.init();
+playerTwo.init();
+
+render();
