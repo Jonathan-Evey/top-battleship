503d5f791f73175812097c1bc4632d28ddc25b64
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-07-27 10:45:58 -0700

    change to launch missile function to check for pc or user player

--- src/player.js
+++ src/player.js
@@ -1,7 +1,14 @@
 const Player = (computer, playerInput) => {
 	const isComputer = computer;
 	const name = playerInput;
-	const launchMissile = () => {};
+	const launchMissile = (coordinates) => {
+		if (isComputer === true) {
+			return console.log("PC Makes pisk");
+		}
+		if (isComputer === false) {
+			return console.log("select and fire");
+		}
+	};
 
 	return {
 		name,
