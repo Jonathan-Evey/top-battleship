09dcaf8985d0e94325a334224b5827f85c718523
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-07-31 10:34:49 -0700

    add dialog templates to show outcome of shots and the next players turn

--- src/DOMElements.js
+++ src/DOMElements.js
@@ -1,16 +1,19 @@
 const HTML = (function () {
-	const startingElements = {
-		HTMLHEAD: document.head,
+	const PERMANENT = {
 		HTMLBODY: document.body,
 	};
 
-	const userInputs = {
+	const startingElements = {
 		form: document.getElementById("start-game-form"),
-		input: document.getElementById("user-name"),
+		inputUserOne: document.getElementById("user-name"),
+		inputUserTwo: document.getElementById("user-two-name"),
+		startGameBtn: document.getElementById("start-game"),
 	};
 
 	const placeShipElements = () => {
 		return (placeShipHTML = {
+			header: document.getElementById("header-title"),
+
 			playerGrid: document.getElementById("player-grid"),
 			enemyGrid: document.getElementById("enemy-grid"),
 
@@ -30,77 +33,73 @@ const HTML = (function () {
 
 			fireBtn: document.getElementById("fire-btn"),
 			launchCoordinates: document.getElementById("coordinates-display"),
+
+			dialog: document.getElementById("dialog"),
+			dialogTitle: document.getElementById("pass-turn-title"),
+			startTurnBtn: document.getElementById("start-turn-btn"),
 		});
 	};
 
-	const btns = {
-		startGame: document.getElementById("start-game"),
-	};
-
-	const placeShipHTMLIndex = {
-		HTMLTemplate: `<header class="game-play-header">
-		<div class="header-container">
-			<div class="header-title-container">
-				<h1>Misstion Control</h1>
-			</div>
-		</div>
-	</header>
-	<main>
-		<section class="player-details">
-			<div class="title-container">
-				<h3>Fleet Map</h3>
-			</div>
-			<div class="player-container">
-				<div class="map-header">
-					<div>1</div>
-					<div>2</div>
-					<div>3</div>
-					<div>4</div>
-					<div>5</div>
-					<div>6</div>
-					<div>7</div>
-					<div>8</div>
-					<div>9</div>
-					<div>10</div>
-				</div>
-				<div class="map-aside">
-					<div>A</div>
-					<div>B</div>
-					<div>C</div>
-					<div>D</div>
-					<div>E</div>
-					<div>F</div>
-					<div>G</div>
-					<div>H</div>
-					<div>I</div>
-					<div>J</div>
-				</div>
-				<div class="play-card" id="player-grid"></div>
-			</div>
-		</section>
-		<aside class="enemy-details">
-			<div class="title-container">
-				<h3>Ship Yard</h3>
-			</div>
-			<div class="ship-container" id="ship-container">
-				<div class="carrier" id="ship"></div>
-				<div class="battleship" id="ship"></div>
-				<div class="destroyer" id="ship"></div>
-				<div class="submarine" id="ship"></div>
-				<div class="patrol" id="ship"></div>
-			</div>
-			<div class="ship-yard-controls-container">
-				<div class="launch-coordinates-display">
-					<p>Ship Direction: <span>Horizontal</span></p>
+	const HTMLTemplates = {
+		placeShipHTMLIndex: `<header class="game-play-header">
+			<div class="header-container">
+				<div class="header-title-container">
+					<h1 id="header-title"></h1>
 				</div>
 			</div>
-		</aside>
-	</main>
-</body>`,
-	};
-
-	const gamePlayHTMLIndex = {
-		HTMLTemplate: `<header class="game-play-header">
+		</header>
+		<main>
+			<section class="player-details">
+				<div class="title-container">
+					<h3>Fleet Map</h3>
+				</div>
+				<div class="player-container">
+					<div class="map-header">
+						<div>1</div>
+						<div>2</div>
+						<div>3</div>
+						<div>4</div>
+						<div>5</div>
+						<div>6</div>
+						<div>7</div>
+						<div>8</div>
+						<div>9</div>
+						<div>10</div>
+					</div>
+					<div class="map-aside">
+						<div>A</div>
+						<div>B</div>
+						<div>C</div>
+						<div>D</div>
+						<div>E</div>
+						<div>F</div>
+						<div>G</div>
+						<div>H</div>
+						<div>I</div>
+						<div>J</div>
+					</div>
+					<div class="play-card" id="player-grid"></div>
+				</div>
+			</section>
+			<aside class="enemy-details">
+				<div class="title-container">
+					<h3>Ship Yard</h3>
+				</div>
+				<div class="ship-container" id="ship-container">
+					<div class="carrier" id="ship"></div>
+					<div class="battleship" id="ship"></div>
+					<div class="destroyer" id="ship"></div>
+					<div class="submarine" id="ship"></div>
+					<div class="patrol" id="ship"></div>
+				</div>
+				<div class="ship-yard-controls-container">
+					<div class="launch-coordinates-display">
+						<p>Ship Direction: <span>Horizontal</span></p>
+					</div>
+				</div>
+			</aside>
+		</main>`,
+		gamePlayHTMLIndex: `<header class="game-play-header">
 		<div class="header-container">
 			<div class="header-title-container">
 				<h1>Mission Control</h1>
@@ -115,8 +114,8 @@ const HTML = (function () {
 				<button class="btn" id="fire-btn">FIRE</button>
 			</div>
 		</div>
-	</header>
-	<main>
+		</header>
+		<main>
 		<section class="player-details">
 			<div class="title-container">
 				<h3>Fleet Map</h3>
@@ -181,7 +180,7 @@ const HTML = (function () {
 				</div>
 				<div class="enemy-card" id="enemy-grid"></div>
 			</div>
-			<div class="enemy-ship-container" id="enemy-ship-container">
+			<div class="ship-container" id="enemy-ship-container">
 				<div class="carrier" id="ship"></div>
 				<div class="battleship" id="ship"></div>
 				<div class="destroyer" id="ship"></div>
@@ -189,17 +188,31 @@ const HTML = (function () {
 				<div class="patrol" id="ship"></div>
 			</div>
 		</aside>
-	</main>`,
+		
+		</main>
+		<dialog id="dialog">
+	</dialog>`,
+		showOutcomeHTML: `<div class="pass-turn-conatiner">
+		<div class="pass-turn-card">
+			<h4 id="pass-turn-title"></h4>
+		</div>
+	</div>`,
+		passTurnHTML: `<div class="pass-turn-conatiner">
+	<div class="pass-turn-card">
+		<h4 id="pass-turn-title"></h4>
+		<p>Click continue to start your turn</p>
+
+		<button id="start-turn-btn">Continue</button>
+	</div>
+</div>`,
 	};
 
 	return {
+		PERMANENT,
 		startingElements,
 		placeShipElements,
 		gamePlayElements,
-		userInputs,
-		btns,
-		placeShipHTMLIndex,
-		gamePlayHTMLIndex,
+		HTMLTemplates,
 	};
 })();
 
